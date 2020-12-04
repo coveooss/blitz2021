42742f2e67c499a61f15b9624121e9505e37ddb7
André Thériault <tedre191@gmail.com>
2020-12-04 15:41:39 -0500

    to do tests

---
+++ test/colony.test.ts
@@ -0,0 +1,13 @@
+describe('Colony', () => {
+    describe('buy unit', () => {
+        it.todo('should throw if there is not enough blitzium available');
+        it.todo('should add the unit to the colony with the proper cost');
+    });
+
+    describe('apply command', () => {
+        it.todo('should send the move command to the proper unit');
+        it.todo('should send the attack command to the proper unit');
+        it.todo('should send the pickup command to the proper unit');
+        it.todo('should allow only one buy command per tick');
+    });
+});
---
+++ test/unit.test.ts
@@ -0,0 +1,19 @@
+describe('Unit', () => {
+    describe('move', () => {
+        it.todo('should throw if there is not path possble');
+        it.todo('should move to the first node of the path towards the target');
+        it.todo('should set the computed path');
+    });
+
+    describe('attack', () => {
+        it.todo('should kill the target');
+        it.todo('should throw if target is not adjacent');
+    });
+
+    describe('pick up', () => {
+        it.todo('should pick up the blitzium from the target');
+        it.todo('should throw if target is not adjacent');
+        it.todo('should throw if the unit is full');
+        it.todo('should only pick up the amount to fill its cargo');
+    })
+});
