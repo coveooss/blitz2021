af2bdc0d85b40b092e117f6874b710022151e31e
Frederic Boutin <fboutin2@coveo.com>
2021-01-07 11:22:51 -0500

    Lower price increase, lower max cargo for transporter and miner

--- game/src/game/config.ts
+++ game/src/game/config.ts
@@ -3,8 +3,8 @@ export class UNIT {
     public static OUTLAW_COST = 150;
     public static CART_COST = 25;
 
-    public static MAX_MINER_CARGO = 50;
-    public static MAX_TRANSPORTER_CARGO = 50;
+    public static MAX_MINER_CARGO = 20;
+    public static MAX_TRANSPORTER_CARGO = 20;
 
     public static MAX_MINER_MOVE_CARGO = 5;
 
@@ -15,5 +15,5 @@ export class UNIT {
 export class COLONY {
     public static START_BALANCE = 25;
     public static SAFE_ZONE_RADIUS = 3;
-    public static UNIT_MULTIPLIER = 1.5;
+    public static UNIT_MULTIPLIER = 1.1;
 }
